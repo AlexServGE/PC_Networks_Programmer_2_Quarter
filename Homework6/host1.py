29ffdb4ee98c5d7d7e3604de9e0fbe6984a8f07e
AlexServGE <alexander.sergeevru@yandex.ru>
2023-02-13 12:27:33 +0300

    Homework6 is ready

---
+++ Homework6/host1.py
@@ -0,0 +1,29 @@
+import socket
+import threading
+
+host_socket = socket.socket()
+server_socket = ('127.0.0.1', 50007)
+
+host_socket.connect(server_socket)
+
+nickname = 'AlexServGE_1'
+host_socket.send(nickname.encode(encoding='ascii'))
+
+
+def send_mess():
+    while True:
+        mes = input()
+        text_to_send = f'{nickname}:{mes}'.encode(encoding='ascii')
+        host_socket.send(text_to_send)
+
+
+def get_mess():
+    while True:
+        text = host_socket.recv(1024)
+        print(text.decode(encoding='ascii'))
+
+
+send_thread = threading.Thread(target=send_mess)
+get_thread = threading.Thread(target=get_mess)
+send_thread.start()
+get_thread.start()
